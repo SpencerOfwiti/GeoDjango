1fa6a18adb612eb317f6e14e5de8042f0011647c
SpencerOfwiti <maxspencer56@gmail.com>
2019-11-28 11:41:46 +0300

    Created view and template showing the 20 closest shops from a users location.

--- shops/views.py
+++ shops/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.gis.geos import fromstr
+from django.contrib.gis.db.models.functions import Distance
+from .models import Shop
+from django.contrib.gis.geos.point import Point
 
+longitude = 37.016033
+latitude = -1.107004
+user_location = Point(longitude, latitude, srid=4326)
 # Create your views here.
+
+
+class ShopList(generic.ListView):
+    model = Shop
+    context_object_name = 'shops'
+    queryset = Shop.objects.annotate(distance=Distance(
+        'location', user_location)
+    ).order_by('distance')[0:20]
+    template_name = 'shops/index.html'
