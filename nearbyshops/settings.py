6bf657d70f7f86ea42a652f811faf98749f778fc
Spencer Ofwiti <maxspencer56@gmail.com>
2020-05-07 16:04:04 +0300

    Update settings.py

--- nearbyshops/settings.py
+++ nearbyshops/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&2!a+%d^ike&7uaqt^y+r_ndy7bseuxngb-2-$2m89fmsjgt@0'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
